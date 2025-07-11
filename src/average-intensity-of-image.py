62b75440a3ccdc5a813ef31638d2ac0a95af655a
des1gnerw1tch <zachcanon@gmail.com>
2025-07-11 13:21:06 -0400

    Added error handling for wrong file type and missing image

--- src/average-intensity-of-image.py
+++ src/average-intensity-of-image.py
@@ -8,8 +8,16 @@ app = Flask(__name__)
 def get_intensity_of_image():
     print(request.files)
     file = request.files.get('image')
+
+    if (file is None):
+        return jsonify({"error": "Missing image file. Make sure to include image file in the 'image' field of the POST request"}), 400
+    
     img_file_bytes = np.frombuffer(file.read(), np.uint8)
     color = cv2.imdecode(img_file_bytes, cv2.IMREAD_COLOR)
+
+    if (color is None):
+        return jsonify({"error": "Error when decoding image, make sure image file is valid"}), 400
+    
     gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
 
     color_intensities = cv2.mean(color)
